c9f7f6fa7aa46e71338545eea85215fef0f18548
pierre delestre <delestre.pierre@gmail.com>
2022-10-08 09:18:31 +0100

    class and constructor training

---
+++ TrainingApp1/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingApp1
+{
+    internal class Car
+    {
+        public string petName;
+        public int currSpeed;
+
+        public Car()
+        {
+            petName = "Chuck";
+            currSpeed = 10;
+        }
+
+        public Car(string pet)
+        {
+            petName = pet;
+        }
+
+        public Car(string pet, int speed)
+        {
+            petName = pet;
+            currSpeed = speed;
+        }
+
+        public void PrintState() => Console.WriteLine($"{petName} is going {currSpeed} kms/h");
+
+        public void SpeedUp(int delta) => currSpeed += delta;
+    }
+}
--- TrainingApp1/Program.cs
+++ TrainingApp1/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TrainingApp1;
 
+var auto = new Car();
+
+for (int i = 0; i < 10; i++)
+{
+    auto.SpeedUp(5);
+    auto.PrintState();
+}
+
+Console.WriteLine();
+
+var otherCar = new Car("Mary");
+otherCar.PrintState();
+
+var anotherCar = new Car("John", 120);
+anotherCar.PrintState();
+
+Console.ReadKey();
